04b4892e8c2a33bb8f3c69b94e6f5fca60e96a70
Alexandre Halas <alexandrehalas@gmail.com>
2023-08-10 16:58:09 -0300

    Default parameters

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+// ES6 default parameters
+const createBooking = function(flightNumber, passengersNumber = 1, price = 100 * passengersNumber) {
+
+    // ES5 default parameters
+    // passengersNumber = passengersNumber || 1;
+    // price = price || 300;
+
+    const booking = {
+        flightNumber,
+        passengersNumber,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123')
+createBooking('LH456', 2)
+createBooking('LH789', 10, 1000)
+
+// how set third parameter and leave second parameter as default value?
+
+createBooking('LH900', undefined, 2000)
